a2f36f3aab99813eed77308ea2638bebce466b9c
maksymshemet <shemet.max92@gmail.com>
2023-12-10 14:04:20 +0200

    Update CollectionExtensions.cs
    + IsNotNullNotEmpty
    + arrays overload

--- Runtime/CollectionExtensions.cs
+++ Runtime/CollectionExtensions.cs
@@ -17,14 +17,20 @@ namespace DarkDynamics.Andromeda.Extensions.Runtime
         }
         
         public static bool IsEmpty<T>(this List<T> list) => list.Count == 0;
-        
+        public static bool IsEmpty<T>(this T[] array) => array.Length == 0;
         public static bool IsEmpty<T>(this HashSet<T> list) => list.Count == 0;
         
-        public static bool IsNotEmpty<T>(this List<T> list) => list.Count > 0;
-        
-        public static bool IsNotEmpty<T>(this HashSet<T> list) => list.Count > 0;
+        public static bool IsNotEmpty<T>(this List<T> list) => !IsEmpty(list);
+        public static bool IsNotEmpty<T>(this HashSet<T> list) => !IsEmpty(list);
+        public static bool IsNotEmpty<T>(this T[] array) => !IsEmpty(array);
+
+        public static bool IsNullOrEmpty<T>(this List<T> list) => list == null || IsEmpty(list);
+        public static bool IsNullOrEmpty<T>(this HashSet<T> list) => list == null || IsEmpty(list);
+        public static bool IsNullOrEmpty<T>(this T[] array) => array == null || IsEmpty(array);
         
         public static bool IsNotNullNotEmpty<T>(this List<T> list) => list is { Count: > 0 };
+        public static bool IsNotNullNotEmpty<T>(this HashSet<T> list) => list is { Count: > 0 };
+        public static bool IsNotNullNotEmpty<T>(this T[] array) => array is { Length: > 0 };
 
         public static bool TryGetAndRemove<TKey, TValue>(this Dictionary<TKey, TValue> dict, in TKey key,
             out TValue value)
